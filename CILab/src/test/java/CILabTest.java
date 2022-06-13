f0f216fa99e26bef2c33b473c12b0c3e3160ce30
Giang Ngo <gnngo2502@gmail.com>
2022-06-13 16:19:48 -0700

    test

--- CILab/src/test/java/CILabTest.java
+++ CILab/src/test/java/CILabTest.java
@@ -2,7 +2,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CILabTest {
 
@@ -19,16 +19,40 @@ public class CILabTest {
     }
 
     @Test
+    public void getStringTest() {
+        String result = myString.getString();
+        assertEquals(null, result);
+    }
+
+    //return false. no uppercase char
+    @Test
     public void detectCapitalUseTest1() {
         myString.setString("mine");
-        boolean result = myString.detectCapitalUse();
-
+        assertEquals(false, myString.detectCapitalUse());
 
     }
+    //return true if it has an uppercase letter
     @Test
     public void detectCapitalUseTest2() {
-        fail("Not yet implemented");
+        myString.setString("America");
+        assertEquals(true, myString.detectCapitalUse());
     }
-
-
+    // testing with number if it returns false
+    @Test
+    public void detectCapitalUseTest3() {
+        myString.setString("10099");
+        assertEquals(false, myString.detectCapitalUse());
+    }
+    //testing whole string uppercase
+    @Test
+    public void detectCapitalUseTest4() {
+        myString.setString("WA");
+        assertEquals(true, myString.detectCapitalUse());
+    }
+    //return false with empty string
+    @Test
+    public void detectCapitalUseTest5() {
+        myString.setString("");
+        assertEquals(false, myString.detectCapitalUse());
+    }
 }
