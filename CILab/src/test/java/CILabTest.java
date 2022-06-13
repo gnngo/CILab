b7cf06149ecd044f02811cca1a47e4e99759bb25
Giang Ngo <gnngo2502@gmail.com>
2022-06-13 15:23:30 -0700

    create

---
+++ CILab/src/test/java/CILabTest.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class CILabTest {
+
+    private CILabInterface myString;
+
+    @BeforeEach
+    public void setUp() {
+        myString = new CILab();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        myString = null;
+    }
+
+    @Test
+    public void detectCapitalUseTest1() {
+        myString.setString("mine");
+        boolean result = myString.detectCapitalUse();
+
+
+    }
+    @Test
+    public void detectCapitalUseTest2() {
+        fail("Not yet implemented");
+    }
+
+
+}
